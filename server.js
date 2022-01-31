2844bf00450ef825e2d1f88b0ff3bd2fa2f7ab0c
LidoKing <kinglido203@gmail.com>
2022-02-01 00:57:47 +0800

    added extra layer of checking url (protocol check)

--- server.js
+++ server.js
@@ -43,42 +43,48 @@ app.get('/api/hello', function(req, res) {
 
 app.post('/api/shorturl', (req, res) => {
   let original = req.body.url;
+  let extractProtocol = original.split(":");
   let url_object;
 
-  try {
-    url_object = new URL(original); // Returns object with properties - hostname, path, etc ...
+  if (extractProtocol[0] == "https" || extractProtocol[0] == "http") {
+    try {
+      // Returns object with properties - hostname, path, etc ...
+      url_object = new URL(original);
 
-    // Check if url is valid
-    dns.lookup(url_object.hostname, (err) => {
-      if (err) {
-        console.log(err);
-        res.json({ error: 'invalid url' });
-      }
-      else{
-        console.log('Valid url');
+      // Check if url is valid
+      dns.lookup(url_object.hostname, (err) => {
+        if (err) {
+          console.log(err);
+          res.json({ error: 'invalid url' });
+        }
+        else{
+          console.log('Valid url');
 
-        // Check if url is registered already
-        url.findOne({ original_url: original }, (err, data) => {
-          if (err) return console.log(err);
+          // Check if url is registered already
+          url.findOne({ original_url: original }, (err, data) => {
+            if (err) return console.log(err);
 
-          // Check if document exists
-          if (!data) {
-            // Add url to database
-            url.create({ original_url: original }, (err, data) => {
-              if (err) return console.log(err);
-              console.log(data);
+            // Check if document exists
+            if (!data) {
+              // Add url to database
+              url.create({ original_url: original }, (err, data) => {
+                if (err) return console.log(err);
+                console.log(data);
+                res.json({ original_url: original, short_url: data.shortenId });
+              });
+            }
+            else {
               res.json({ original_url: original, short_url: data.shortenId });
-            });
-          }
-          else {
-            res.json({ original_url: original, short_url: data.shortenId });
-          }
-        });
-      }
-    });
-  } catch (e) {
-    console.log(e);
-    res.json({ error: 'invalid url' });
+            }
+          });
+        }
+      });
+    } catch (e) {
+      console.log(e);
+      res.json({ error: 'invalid url' });
+    }
+  } else {
+    res.json({ error: "invalid url" });
   }
 });
 
